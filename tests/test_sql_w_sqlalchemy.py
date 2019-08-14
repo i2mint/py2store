d0d7e59c0ae9c0b383a4088db757e3271b7f3127
Mikhail Savushkin <kulv3r@gmail.com>
2019-08-14 16:55:57 +0300

    Base tests added, SQLAlchemy tests adopted.

--- tests/test_sql_w_sqlalchemy.py
+++ tests/test_sql_w_sqlalchemy.py
@@ -1,95 +1,49 @@
 import os
 
-import pytest
-
-from py2store.stores.sql_w_sqlalchemy import SQLAlchemyPersister, SQLAlchemyStore
-
+from py2store.stores.sql_w_sqlalchemy import SQLAlchemyStore
+from tests.base_test import BaseStoreTest
 
 SQLITE_DB_PATH = 'test.db'
 SQLITE_DB_URI = f'sqlite:///{SQLITE_DB_PATH}'
+SQLITE_TABLE_NAME = 'test_table'
 
 
-@pytest.fixture(scope='function')
-def sqlite_db():
-    yield
+def clean_db_path(path=SQLITE_DB_PATH):
     try:
-        os.remove(SQLITE_DB_PATH)
+        os.remove(path)
     except FileNotFoundError:
         pass
 
 
-class TestSQLAlchemy:
-    key = {
+class TestSQLAlchemyTupleStore(BaseStoreTest):
+    key_dict = {
         'first_name': 'Yuri',
         'last_name': 'Gagarin',
     }
-    key_as_tuple = tuple(key.values())
+    key = tuple(key_dict.values())
 
-    data = {
+    data_dict = {
         'height': '185sm',
         'weight': '80kg',
         'is_hero': 'yes he is',
     }
-    data_as_tuple = tuple(data.values())
+    data = tuple(data_dict.values())
 
-    joined_values = {**key, **data}
+    joined_values = {**key_dict, **data_dict}
 
-    def test_persister(self, sqlite_db):
-        sql_dict = SQLAlchemyPersister(
-            db_uri='sqlite:///test.db',
-            collection_name='tmp',
-            key_fields=list(self.key.keys()),
-            data_fields=list(self.data.keys()),
+    @classmethod
+    def teardown_class(cls):
+        clean_db_path()
+
+    def test_crud(self):
+        store = SQLAlchemyStore(
+            db_uri=SQLITE_DB_URI,
+            collection_name=SQLITE_TABLE_NAME,
+            key_fields=list(self.key_dict.keys()),
+            data_fields=list(self.data_dict.keys()),
         )
 
-        # Assigning a value to a new key...
-        sql_dict[self.key] = self.data
-        assert len(sql_dict) == 1
-
-        for k, v in self.joined_values.items():
-            object_from_db = sql_dict[self.key]
-            assert getattr(object_from_db, k) == v
-
-        assert sql_dict.get(self.key) == object_from_db
-
-        # Testing s.get with default...
-        assert sql_dict.get({'first_name': 'totally not Yuri'}, 'default val') == 'default val'
-
-        # Testing __contains__ again...
-        assert self.key in sql_dict
-
-        # Testing deleting key...
-        del sql_dict[self.key]
-        assert len(sql_dict) == 0
-
-        # See that key is not in store (and testing __contains__)...
-        assert self.key not in sql_dict
-        assert len(sql_dict) == 0
-
-    def test_store(self, sqlite_db):
-        sql_dict = SQLAlchemyStore(
-            db_uri='sqlite:///test.db',
-            collection_name='tmp',
-            key_fields=list(self.key.keys()),
-            data_fields=list(self.data.keys()),
-        )
-
-        # Assigning a value to a new key...
-        sql_dict[self.key_as_tuple] = self.data_as_tuple
-        assert len(sql_dict) == 1
-        assert sql_dict[self.key_as_tuple] == self.data_as_tuple
-        assert sql_dict.get(self.key_as_tuple) == self.data_as_tuple
-
-        # Testing s.get with default...
-        assert sql_dict.get('totally not Yuri', 'default val') == 'default val'
-
-        # Testing __contains__...
-        assert self.key_as_tuple in sql_dict
-
-        # Testing deleting key...
-        del sql_dict[self.key_as_tuple]
-        assert len(sql_dict) == 0
-
-        # See that key is not in store (and testing __contains__)...
-        assert self.key_as_tuple not in sql_dict
-        assert len(sql_dict) == 0
+        self._test_create(store)
+        self._test_read(store)
+        self._test_update(store)
+        self._test_delete(store)
