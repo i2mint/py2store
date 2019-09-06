fcf865ed9f3a862603d98c907695126abe23d459
Mikhail Savushkin <kulv3r@gmail.com>
2019-09-06 16:26:43 +0300

    ArangoDB major fix.

--- py2store/persisters/_arangodb_in_progress.py
+++ py2store/persisters/_arangodb_in_progress.py
@@ -3,6 +3,7 @@ from py2store.util import ModuleNotFoundErrorNiceMessage
 
 with ModuleNotFoundErrorNiceMessage():
     from pyArango.connection import Connection
+    from pyArango.theExceptions import DocumentNotFoundError
 
 
 class ArangoDbPersister(Persister):
@@ -61,17 +62,25 @@ class ArangoDbPersister(Persister):
             url='http://127.0.0.1:8529',
             db_name='py2store',
             collection_name='test',
-            key_fields=('key',)  # _id, _key and _rev are reserved by db
+            key_fields=('key',),  # _id, _key and _rev are reserved by db
+            key_fields_separator='::',
     ):
-        self._connection = Connection(arangoURL=url, username=user, password=password)
+        self._connection = Connection(
+            arangoURL=url,
+            username=user,
+            password=password,
+        )
+
         self._db_name = db_name
         self._collection_name = collection_name
-        # if db not created
+
+        # If DB not created:
         if not self._connection.hasDatabase(self._db_name):
             self._connection.createDatabase(self._db_name)
 
         self._adb = self._connection[self._db_name]
-        # if collection not created
+
+        # If collection not created:
         if not self._adb.hasCollection(self._collection_name):
             self._collection = self._adb.createCollection(name=self._collection_name)
 
@@ -81,40 +90,84 @@ class ArangoDbPersister(Persister):
             key_fields = (key_fields,)
 
         self._key_fields = key_fields
+        self._key_fields_separator = key_fields_separator
 
-    def __fetchitem__(self, k):
-        f = self._collection.fetchFirstExample(k)
-        if f is not None and len(f) == 1:
-            return f[0]
+    def _make_key(self, keys_dict):
+        """
+        Convert a dict of keys into a real key-string by joining dict values in a predefined order.
 
-        return None
+        DB requirements for the key:
+            The key must be a string value.
+            Keys are case-sensitive.
+            Numeric keys are not allowed.
+            The key must be from 1 byte to 254 bytes long.
+            It must consist of:
+                - letters a-z (lower or upper case),
+                - digits 0-9
+                - any of the following characters: _ - : . @ ( ) + , = ; $ ! * ' %
 
-    def __getitem__(self, k):
-        f = self.__fetchitem__(k)
-        if f is not None:
-            d = f.getStore()
-            # exclude reserved keys and corresponded values
-            d = {x: d[x] for x in d if x not in self._reserved and x not in self._key_fields}
-            return d
-        else:
-            raise KeyError(f"No document found for query: {k}")
+            Any other characters cannot be used inside key values.
+        """
+        key_values = [keys_dict[key_label] for key_label in self._key_fields]
+        key_str = self._key_fields_separator.join(key_values)
+        return key_str
 
-    def __setitem__(self, k, v):
-        doc = self._collection.createDocument(dict(k, **v))
+    def _split_key(self, joined_key_str):
+        """
+        Convert a key-string used by DB internally
+        into a user-friendly dict of key labels and values.
+        """
+        key_values = joined_key_str.split(self._key_fields_separator)
+        keys_dict = dict(zip(self._key_fields, key_values))
+        return keys_dict
+
+    def __fetchitem__(self, keys_dict):
+        key = self._make_key(keys_dict)
+        try:
+            return self._collection[key]
+        except DocumentNotFoundError:
+            raise KeyError(f"No document found for query: {keys_dict}")
+
+    def __getitem__(self, keys_dict):
+        item = self.__fetchitem__(keys_dict)
+        doc = item.getStore()
+
+        # todo (Mike): maybe move this cleanup to a base Arango Store?
+        # exclude reserved keys and corresponded values
+        data = {
+            key: doc[key]
+            for key in doc
+            if key not in self._reserved and
+               key not in self._key_fields
+        }
+        return data
+
+    def __setitem__(self, keys_dict, values_dict):
+        try:
+            doc = self.__fetchitem__(keys_dict)
+        except KeyError:
+            doc = self._collection.createDocument()
+            doc._key = self._make_key(keys_dict)
+
+        for k, v in values_dict.items():
+            doc[k] = v
+
         doc.save()
 
-    def __delitem__(self, k):
-        if len(k) > 0:
-            f = self.__fetchitem__(k)
-            if f is not None:
-                return f.delete()
-
-        raise KeyError(f"You can't removed that key: {k}")
+    def __delitem__(self, keys_dict):
+        doc = self.__fetchitem__(keys_dict)
+        doc.delete()
 
     def __iter__(self):
         docs = self._collection.fetchAll()
 
-        yield from [{x: d[x] for x in d.getStore() if x in self._key_fields} for d in docs]
+        yield from (
+            {
+                key_name: doc[key_name]
+                for key_name in self._key_fields
+            }
+            for doc in docs
+        )
 
     def __len__(self):
         return self._collection.count()
