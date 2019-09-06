31440daec8e56c495c4a162c7f6668965db444da
Mikhail Savushkin <kulv3r@gmail.com>
2019-09-06 16:27:38 +0300

    Base tests for Persisters and Stores significantly improved. SQLAlchemy tests adjusted.

--- tests/test_sql_w_sqlalchemy.py
+++ tests/test_sql_w_sqlalchemy.py
@@ -1,49 +1,30 @@
-import os
+from py2store.persisters.sql_w_sqlalchemy import SQLAlchemyPersister
+from py2store.stores.sql_w_sqlalchemy import SQLAlchemyTupleStore, SQLAlchemyStore
+from tests.base_test import BaseStoreTest, BasePersisterTest, BaseKeyTupleStoreTest, BaseTupleStoreTest
 
-from py2store.stores.sql_w_sqlalchemy import SQLAlchemyTupleStore
-from tests.base_test import BaseStoreTest
-
-SQLITE_DB_PATH = 'test.db'
-SQLITE_DB_URI = f'sqlite:///{SQLITE_DB_PATH}'
+SQLITE_DB_URI = 'sqlite:///:memory:'
 SQLITE_TABLE_NAME = 'test_table'
 
 
-def clean_db_path(path=SQLITE_DB_PATH):
-    try:
-        os.remove(path)
-    except FileNotFoundError:
-        pass
+class TestSQLAlchemyPersister(BasePersisterTest):
+    db = SQLAlchemyPersister(
+        db_uri=SQLITE_DB_URI,
+        collection_name=SQLITE_TABLE_NAME,
+        key_fields=list(BasePersisterTest.key.keys()),
+        data_fields=list(BasePersisterTest.data.keys()),
+    )
+
+    def _assert_eq(self, obj_from_db, data):
+        """ Custom comparison by attributes, since persister returns objects. """
+        for data_key, data_val in data.items():
+            val_from_db = getattr(obj_from_db, data_key)
+            assert val_from_db == data_val
 
 
-class TestSQLAlchemyTupleStore(BaseStoreTest):
-    key_dict = {
-        'first_name': 'Yuri',
-        'last_name': 'Gagarin',
-    }
-    key = tuple(key_dict.values())
-
-    data_dict = {
-        'height': '185sm',
-        'weight': '80kg',
-        'is_hero': 'yes he is',
-    }
-    data = tuple(data_dict.values())
-
-    joined_values = {**key_dict, **data_dict}
-
-    @classmethod
-    def teardown_class(cls):
-        clean_db_path()
-
-    def test_crud(self):
-        store = SQLAlchemyTupleStore(
-            db_uri=SQLITE_DB_URI,
-            collection_name=SQLITE_TABLE_NAME,
-            key_fields=list(self.key_dict.keys()),
-            data_fields=list(self.data_dict.keys()),
-        )
-
-        self._test_create(store)
-        self._test_read(store)
-        self._test_update(store)
-        self._test_delete(store)
+class TestSQLAlchemyTupleStore(BaseTupleStoreTest):
+    db = SQLAlchemyTupleStore(
+        db_uri=SQLITE_DB_URI,
+        collection_name=SQLITE_TABLE_NAME,
+        key_fields=BaseTupleStoreTest.key_fields,
+        data_fields=BaseTupleStoreTest.data_fields,
+    )
