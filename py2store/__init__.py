1a0cd3649fb6577e8579083c8110d69c60947fbc
twhalen <thor@otosense.com>
2019-10-07 14:47:19 -0700

    added user_configs and user_defaults root __init__ variables, who at this point are defined by files '~/.py2store_configs.json' and '~/.py2store_defaults.json' respectively, if these files exist (and resolve to a dict without an exception

--- py2store/__init__.py
+++ py2store/__init__.py
@@ -6,3 +6,20 @@ from py2store.stores.local_store import QuickStore, QuickBinaryStore, QuickTextS
 from py2store.base import Store
 
 file_sep = os.path.sep
+
+user_configs = {}
+user_defaults = {}
+
+try:
+    import json
+
+    config_filepath = os.path.expanduser('~/.py2store_configs.json')
+    if os.path.isfile(config_filepath):
+        user_configs = json.load(open(config_filepath))
+
+    defaults_filepath = os.path.expanduser('~/.py2store_defaults.json')
+    if os.path.isfile(defaults_filepath):
+        user_defaults = json.load(open(defaults_filepath))
+
+except:
+    pass
