a4710a6814bb1169c3502798a6c8c37e7d3e6529
Mikhail Savushkin <kulv3r@gmail.com>
2019-11-01 17:25:32 +0300

    Minor ArangoDB Test docstring change (to be alike Mongo's).

--- tests/test_arangodb.py
+++ tests/test_arangodb.py
@@ -1,8 +1,8 @@
 """
 How to run a test ArangoDB instance locally with a Docker container:
 
-docker rm -f arangodb-instance && \
-docker run -e ARANGO_ROOT_PASSWORD=somepassword -p 8529:8529 -d --name arangodb-instance arangodb && \
+docker rm -f arangodb-test && \
+docker run -e ARANGO_ROOT_PASSWORD=somepassword -p 8529:8529 -d --name arangodb-test arangodb && \
 sleep 10 && \
 pytest tests/test_arangodb.py
 """
