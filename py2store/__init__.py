460902629e0cf032e45cacd82725b2030f002e83
twhalen <thor@otosense.com>
2019-11-22 10:00:40 -0800

    ZipReader: A KvReader for zip files

--- py2store/__init__.py
+++ py2store/__init__.py
@@ -13,6 +13,7 @@ from py2store.trans import wrap_kvs
 from py2store.access import user_configs_dict, user_configs, user_defaults_dict, user_defaults
 
 from py2store.stores.local_store import PickleStore  # consider deprecating and use LocalPickleStore instead?
+from py2store.persisters.local_files import ZipReader
 
 with ModuleNotFoundIgnore():
     from py2store.stores.s3_store import S3BinaryStore, S3TextStore, S3PickleStore
--- py2store/persisters/local_files.py
+++ py2store/persisters/local_files.py
@@ -384,6 +384,129 @@ class DirCollection(FileSysCollection):
     def __contains__(self, k):
         return k.startswith(self.rootdir) and os.path.isdir(k) and self.is_valid_key(k)
 
+
+from zipfile import ZipFile
+import os
+import inspect
+from functools import wraps
+from py2store.util import lazyprop
+
+
+def func_conjunction(func1, func2):
+    """Returns a function that is equivalent to lambda x: func1(x) and func2(x)"""
+    # Should assert that the input paramters of func1 and func2 are the same
+    assert inspect.signature(func1).parameters == inspect.signature(func2).parameters
+
+    @wraps(func2)
+    def func(*args, **kwargs):
+        return func1(*args, **kwargs) and func2(*args, **kwargs)
+
+    return func
+
+
+def take_everything(fileinfo):
+    return True
+
+
+# TODO: The way prefix and file_info_filt is handled is not efficient
+# (especially when getitem returns subdirs)
+class ZipReader(KvReader):
+    """A KvReader to read the contents of a zip file
+
+    Examples:
+        # >>> s = ZipReader('/path/to/some_zip_file.zip')
+        # >>> len(s)
+        # 53432
+        # >>> list(s)[:3]  # the first 3 elements (well... their keys)
+        # ['odir/', 'odir/app/', 'odir/app/data/']
+        # >>> list(s)[-3:]  # the last 3 elements (well... their keys)
+        # ['odir/app/data/audio/d/1574287049078391/m/Ctor.json',
+        #  'odir/app/data/audio/d/1574287049078391/m/intensity.json',
+        #  'odir/app/data/run/status.json']
+        # >>> # getting a file (note that by default, you get bytes, so need to decode)
+        # >>> s['odir/app/data/run/status.json'].decode()
+        # b'{"test_phase_number": 9, "test_phase": "TestActions.IGNORE_TEST", "session_id": 0}'
+        # >>> # when you ask for the contents for a key that's a directory,
+        # >>> # you get a ZipReader filtered for that prefix:
+        # >>> s['odir/app/data/audio/']
+        # ZipReader('/path/to/some_zip_file.zip', 'odir/app/data/audio/', {}, <function take_everything at 0x1538999e0>)
+        # >>> # Often, you only want files (not directories)
+        # >>> # You can filter directories out using the file_info_filt argument
+        # >>> s = ZipReader('/path/to/some_zip_file.zip', file_info_filt=ZipReader.FILES_ONLY)
+        # >>> len(s)  # compare to the 53432 above, that contained dirs too
+        # 53280
+        # >>> list(s)[:3]  # first 3 keys are all files now
+        # ['odir/app/data/plc/d/1574304926795633/d/1574305026895702',
+        #  'odir/app/data/plc/d/1574304926795633/d/1574305276853053',
+        #  'odir/app/data/plc/d/1574304926795633/d/1574305159343326']
+        # >>>
+        # >>> # ZipReader.FILES_ONLY and ZipReader.DIRS_ONLY are just convenience filt functions
+        # >>> # Really, you can provide any custom one yourself.
+        # >>> # This filter function should take a ZipInfo object, and return True or False.
+        # >>> # (https://docs.python.org/3/library/zipfile.html#zipfile.ZipInfo)
+        # >>>
+        # >>> import re
+        # >>> p = re.compile('audio.*\.json$')
+        # >>> my_filt_func = lambda fileinfo: bool(p.search(fileinfo.filename))
+        # >>> s = ZipReader('/Users/twhalen/Downloads/2019_11_21.zip', file_info_filt=my_filt_func)
+        # >>> len(s)
+        # 48
+        # >>> list(s)[:3]
+        # ['odir/app/data/audio/d/1574333557263758/m/Ctor.json',
+        #  'odir/app/data/audio/d/1574333557263758/m/intensity.json',
+        #  'odir/app/data/audio/d/1574288084739961/m/Ctor.json']
+    """
+
+    def __init__(self, zip_file, prefix='', open_kws=None, file_info_filt=take_everything):
+        self.open_kws = open_kws or {}
+        self.file_info_filt = file_info_filt
+        self.prefix = prefix
+        if not isinstance(zip_file, ZipFile):
+            if isinstance(zip_file, dict):
+                zip_file = ZipFile(**zip_file)
+            elif isinstance(zip_file, (tuple, list)):
+                zip_file = ZipFile(*zip_file)
+            else:
+                zip_file = ZipFile(zip_file)
+        self.zip_file = zip_file
+
+    @lazyprop
+    def info_for_key(self):
+        return {x.filename: x for x in self.zip_file.infolist()
+                if x.filename.startswith(self.prefix) and self.file_info_filt(x)
+                }
+
+    def __iter__(self):
+        # using zip_file.infolist(), we could also filter for info (like directory/file)
+        yield from self.info_for_key.keys()
+
+    def __getitem__(self, k):
+        if not self.info_for_key[k].is_dir():
+            with self.zip_file.open(k, **self.open_kws) as fp:
+                return fp.read()
+        else:  # is a directory
+            return self.__class__(self.zip_file, k, self.open_kws, self.file_info_filt)
+
+    def __len__(self):
+        return len(self.info_for_key)
+
+    @staticmethod
+    def FILES_ONLY(fileinfo):
+        return not fileinfo.is_dir()
+
+    @staticmethod
+    def DIRS_ONLY(fileinfo):
+        return fileinfo.is_dir()
+
+    def __repr__(self):
+        args_str = ', '.join((
+            f"'{self.zip_file.filename}'",
+            f"'{self.prefix}'",
+            f"{self.open_kws}",
+            f"{self.file_info_filt}"
+        ))
+        return f"{self.__class__.__name__}({args_str})"
+
 #
 # if __name__ == '__main__':
 #     from py2store.test.simple import test_local_file_ops
