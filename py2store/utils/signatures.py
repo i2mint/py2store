e10e6500060f7048092161890cb910dac01078e3
twhalen <thor@otosense.com>
2020-01-17 11:06:26 -0800

    Changed default of _merged_signatures_of_func_list to not include the return annotation unless explicitly asked for (explicitly, or specifying the function to extract it from).
    update_signature_with_signatures_from_funcs: input funcs were first in priority: Changed that to be last, but with choice to be first.
    
    Many more doctests for both functions.

--- py2store/utils/signatures.py
+++ py2store/utils/signatures.py
@@ -1,32 +1,55 @@
 from functools import reduce
 from inspect import Signature, Parameter, _empty, _ParameterKind, signature
-from glom import Spec
+from typing import Any
 
 mappingproxy = type(Signature().parameters)
 
-signature_to_dict = Spec({'parameters': 'parameters',
-                          'return_annotation': 'return_annotation'}).glom
+
+# PATTERN: tree crud pattern
+def signature_to_dict(sig: Signature):
+    return {'parameters': sig.parameters, 'return_annotation': sig.return_annotation}
 
 
-def update_signature_with_signatures_from_funcs(funcs):
+# TODO: will we need more options for the priority argument? Like position?
+def update_signature_with_signatures_from_funcs(*funcs, priority: str = 'last'):
     """Make a decorator that will merge the signatures of given funcs to the signature of the wrapped func.
+    By default, the funcs signatures will be placed last, but can be given priority by asking priority = 'first'
+
     >>> def foo(a='a', b: int=0, c=None) -> int: ...
     >>> def bar(b: float=0.0, d: str='hi') -> float: ...
-    >>> @update_signature_with_signatures_from_funcs([foo, bar])
+    >>> def something(y=(1, 2)): ...
+    >>> def another(y=10): ...
+    >>> @update_signature_with_signatures_from_funcs(foo, bar)
     ... def hello(x: str='hi', y=1) -> str:
     ...     pass
     >>> signature(hello)
-    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi', x: str = 'hi', y=1) -> str>
+    <Signature (x: str = 'hi', y=1, a='a', b: float = 0.0, c=None, d: str = 'hi')>
+    >>>
+    >>> # Try a different order and priority == 'first'. Notice the b arg type and default!
+    >>> add_foobar_to_signature_first = update_signature_with_signatures_from_funcs(bar, foo, priority='first')
+    >>> bar_foo_something = add_foobar_to_signature_first(something)
+    >>> signature(bar_foo_something)
+    <Signature (b: int = 0, d: str = 'hi', a='a', c=None, y=(1, 2))>
+    >>> # See how you can reuse the decorator several times
+    >>> bar_foo_another = add_foobar_to_signature_first(another)
+    >>> signature(bar_foo_another)
+    <Signature (b: int = 0, d: str = 'hi', a='a', c=None, y=10)>
     """
-    if callable(funcs):
-        funcs = [funcs]
+    if not isinstance(priority, str):
+        raise TypeError("priority should be a string")
 
-    def wrapper(func):
-        funcs.append(func)
-        func.__signature__ = _merged_signatures_of_func_list(funcs)
-        return func
+    if priority == 'last':
+        def transform_signature(func):
+            func.__signature__ = _merged_signatures_of_func_list([func] + list(funcs))
+            return func
+    elif priority == 'first':
+        def transform_signature(func):
+            func.__signature__ = _merged_signatures_of_func_list(list(funcs) + [func])
+            return func
+    else:
+        raise ValueError("priority should be 'last' or 'first'")
 
-    return wrapper
+    return transform_signature
 
 
 def common_and_diff_argnames(func1: callable, func2: callable) -> dict:
@@ -180,6 +203,8 @@ def set_signature_of_func(func, parameters, *, return_annotation=_empty, __valid
     # Not returning func so it's clear(er) that the function is transformed in place
 
 
+# TODO: It seems the better choice would be to oblige the user to deal with return annotation explicitly
+
 def _merge_sig_dicts(sig1_dict, sig2_dict):
     """Merge two signature dicts. A in dict.update(sig1_dict, **sig2_dict), but specialized for signature dicts.
     If sig1_dict and sig2_dict both define a parameter or return annotation, sig2_dict decides on what the output is.
@@ -222,16 +247,32 @@ def _merge_signatures_of_funcs(func1, func2):
     return _merge_signatures(signature(func1), signature(func2))
 
 
-def _merged_signatures_of_func_list(funcs):
+def _merged_signatures_of_func_list(funcs, return_annotation: Any = _empty):
     """
     >>> def foo(a='a', b: int=0, c=None) -> int: ...
     >>> def bar(b: float=0.0, d: str='hi') -> float: ...
     >>> def hello(x: str='hi', y=1) -> str: ...
+    >>>
+    >>> # Test how the order of the functions affect the order of the parameters
     >>> _merged_signatures_of_func_list([foo, bar, hello])
-    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi', x: str = 'hi', y=1) -> str>
+    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi', x: str = 'hi', y=1)>
     >>> _merged_signatures_of_func_list([hello, foo, bar])
-    <Signature (x: str = 'hi', y=1, a='a', b: float = 0.0, c=None, d: str = 'hi') -> float>
+    <Signature (x: str = 'hi', y=1, a='a', b: float = 0.0, c=None, d: str = 'hi')>
     >>> _merged_signatures_of_func_list([foo, bar, hello])
-    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi', x: str = 'hi', y=1) -> str>
+    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi', x: str = 'hi', y=1)>
+    >>>
+    >>> # Test the return_annotation argument
+    >>> _merged_signatures_of_func_list([foo, bar], list)  # specifying that the return type is a list
+    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi') -> list>
+    >>> _merged_signatures_of_func_list([foo, bar], foo)  # specifying that the return type is a list
+    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi') -> int>
+    >>> _merged_signatures_of_func_list([foo, bar], bar)  # specifying that the return type is a list
+    <Signature (a='a', b: float = 0.0, c=None, d: str = 'hi') -> float>
     """
-    return reduce(_merge_signatures, map(signature, funcs))
+
+    s = reduce(_merge_signatures, map(signature, funcs))
+
+    if return_annotation in funcs:  # then you want the return annotation of a specific func of funcs
+        return_annotation = signature(return_annotation).return_annotation
+
+    return s.replace(return_annotation=return_annotation)
