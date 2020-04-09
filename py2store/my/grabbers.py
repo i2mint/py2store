463464da068453537fc5d959a485b4d56ee6cee2
twhalen <thor@otosense.com>
2020-04-09 12:16:07 -0400

    ipython grabber has html now

--- py2store/my/grabbers.py
+++ py2store/my/grabbers.py
@@ -1,28 +1,6 @@
 from py2store import get_obj
 from functools import wraps
-
-
-def _has_wav_header(b):
-    return len(b) >= 44 and b[:4] == b'RIFF' and b[8:12] == b'WAVE'
-
-
-def ipython_display_val_trans(val):
-    import imghdr
-    from io import BytesIO
-    image_type = imghdr.what(BytesIO(val))  # will be None if it's not an image
-    if image_type is not None:
-        from IPython.display import Image
-        return Image(val)
-    elif _has_wav_header(val):
-        from IPython.display import Audio
-        return Audio(val)
-    else:
-        return val
-
-
-def fullpath(path):
-    import os
-    return os.path.abspath(os.path.expanduser(path))
+from io import BytesIO, StringIO
 
 
 def mk_grabber(*, key_trans=None, val_trans=None):
@@ -33,11 +11,57 @@ def mk_grabber(*, key_trans=None, val_trans=None):
             k = key_trans(k)
         v = get_obj(k, *args, **kwargs)
         if val_trans is not None:
-            return val_trans(v)
+            return val_trans(v, k)
         else:
             return v
 
     return grab
+
+
+def _read_line_and_rewind(readable):
+    comebackto = readable.tell()
+    line = readable.readline()
+    readable.seek(comebackto)
+    return line
+
+
+def _has_wav_header(b):
+    return len(b) >= 44 and b[:4] == b'RIFF' and b[8:12] == b'WAVE'
+
+
+def _is_html(x, key=None):
+    if isinstance(key, str) and len(key) > 4:
+        if 'htm' in key[-4:]:
+            return True
+    else:
+        if isinstance(x, (BytesIO, StringIO)):
+            x = _read_line_and_rewind
+        if isinstance(x, str):
+            return x[:15] == '<!DOCTYPE html>'
+        elif isinstance(x, bytes):
+            return x[:15] == b'<!DOCTYPE html>'
+    return False  # if not returned before
+
+
+def ipython_display_val_trans(val, key=None):
+    from IPython.display import Image, Audio, HTML
+    import imghdr
+    image_type = imghdr.what(BytesIO(val))  # will be None if it's not an image
+    if image_type is not None:
+        return Image(val)
+    elif _has_wav_header(val):
+        return Audio(val)
+    elif _is_html(val, key):
+        if isinstance(val, bytes):
+            val = val.decode()
+        return HTML(val)
+    else:
+        return val
+
+
+def fullpath(path):
+    import os
+    return os.path.abspath(os.path.expanduser(path))
 
 
 DFLT_GRABBER = get_obj
