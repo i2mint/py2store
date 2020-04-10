9914a32c0f59fa20cffbdcbc3a2330410bb235b8
twhalen <thor@otosense.com>
2020-04-10 10:42:35 -0400

    added more docs to items_with_caught_exceptions

--- py2store/errors.py
+++ py2store/errors.py
@@ -5,6 +5,26 @@ def items_with_caught_exceptions(d: Mapping, callback=None, catch_exceptions=(Ke
     """
     Do what Mapping.items() does, but catching exceptions when getting the values for a key.
 
+
+    Some time your `store.items()` is annoying because of some exceptions that happen
+    when you're retrieving some value for some of the keys.
+
+    Yes, if that happens, it's that something is wrong with your store, and yes,
+    if it's a store that's going to be used a lot, you really should build the right store
+    that doesn't have that problem.
+
+    But now that we appeased the paranoid naysayers with that warning, let's get to business:
+    Sometimes you just want to get through the hurdle to get the job done. Sometimes your store is good enough,
+    except for a few exceptions. Sometimes your store gets it's keys from a large pool of possible keys
+    (e.g. github stores or kaggle stores, or any store created by a free-form search seed),
+    so you can't really depend on the fact that all the keys given by your key iterator
+    will give you a value without exception
+    -- especially if you slapped on a bunch of post-processing on the out-coming values.
+
+    So you can right a for loop to iterate over your keys, catch the exceptions, do something with it...
+
+    Or, in many cases, you can just use `items_with_caught_exceptions`.
+
     :param d: Any Mapping
     :param catch_exceptions: A tuple of exceptions that should be caught
     :param callback: A function that will be called on the key and exception object every time an exception is caught
