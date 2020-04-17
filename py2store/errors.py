b92031f154a3ad63f3d0aa900886d2b9166f955b
twhalen <thor@otosense.com>
2020-04-17 12:16:31 -0400

    beefed up items_with_caught_exceptions

--- py2store/errors.py
+++ py2store/errors.py
@@ -1,7 +1,12 @@
 from collections.abc import Mapping
+from inspect import signature
 
 
-def items_with_caught_exceptions(d: Mapping, callback=None, catch_exceptions=(KeyError,)):
+# TODO: More on wrapped_callback: Handle *args too. Make it work with builtins (no signature!)
+# TODO: What about traceback?
+# TODO: Make it a more general and useful store decorator. Trans store into an getitem exception catching store.
+def items_with_caught_exceptions(d: Mapping, callback=None,
+                                 catch_exceptions=(KeyError,), yield_callback_output=False):
     """
     Do what Mapping.items() does, but catching exceptions when getting the values for a key.
 
@@ -27,14 +32,17 @@ def items_with_caught_exceptions(d: Mapping, callback=None, catch_exceptions=(Ke
 
     :param d: Any Mapping
     :param catch_exceptions: A tuple of exceptions that should be caught
-    :param callback: A function that will be called on the key and exception object every time an exception is caught
-    :return: An (key,val) generator with exceptions caught
+    :param callback: A function that will be called every time an exception is caught.
+        The signature of the callback function is required to be:
+            k (key), e (error obj), d (mapping), i (index)
+        but
+    :return: An (key, val) generator with exceptions caught
 
     >>> from collections.abc import Mapping
     >>> class Test(Mapping):  # a Mapping class that has keys 0..9, but raises of KeyError if the key is not even
     ...     n = 10
     ...     def __iter__(self):
-    ...         yield from range(self.n)
+    ...         yield from range(2, self.n)
     ...     def __len__(self):
     ...         return self.n
     ...     def __getitem__(self, k):
@@ -44,22 +52,49 @@ def items_with_caught_exceptions(d: Mapping, callback=None, catch_exceptions=(Ke
     ...             raise KeyError('Not even')
     >>>
     >>> list(items_with_caught_exceptions(Test()))
-    [(0, 0), (2, 2), (4, 4), (6, 6), (8, 8)]
-    >>> list(items_with_caught_exceptions(Test(), callback=print))
-    1 'Not even'
+    [(2, 2), (4, 4), (6, 6), (8, 8)]
+    >>>
+    >>> def my_log(k, e):
+    ...     print(k, e)
+    >>> list(items_with_caught_exceptions(Test(), callback=my_log))
     3 'Not even'
     5 'Not even'
     7 'Not even'
     9 'Not even'
-    [(0, 0), (2, 2), (4, 4), (6, 6), (8, 8)]
+    [(2, 2), (4, 4), (6, 6), (8, 8)]
+    >>> def my_other_log(i):
+    ...     print(i)
+    >>> list(items_with_caught_exceptions(Test(), callback=my_other_log))
+    1
+    3
+    5
+    7
+    [(2, 2), (4, 4), (6, 6), (8, 8)]
     """
-    for k in d:
+
+    # wrap the input callback so that
+    if callback is not None:
         try:
-            v = d[k]
-            yield k, v
-        except catch_exceptions as e:
-            if callback is not None:
-                callback(k, e)
+            params = signature(callback).parameters
+        except ValueError:
+            def wrapped_callback(k, e, d, i):
+                return callback(k, e, d, i)
+        finally:
+            def wrapped_callback(**kwargs):
+                kwargs = {k: v for k, v in kwargs.items() if k in params}
+                return callback(**kwargs)
+    else:
+        def wrapped_callback(k, e, d, i):
+            pass  # do nothing
+
+    for i, k in enumerate(d):  # iterate over keys of the mapping
+        try:
+            v = d[k]  # try getting the value...
+            yield k, v  # and if you do, yield the (k, v) pair
+        except catch_exceptions as e:  # catch the specific exceptions you requested to catch
+            t = wrapped_callback(k=k, e=e, d=d, i=i)  # call it
+            if yield_callback_output:  # if the user wants the output of the callback
+                yield t  # yield it
 
 
 def _assert_condition(condition, err_msg='', err_cls=AssertionError):
