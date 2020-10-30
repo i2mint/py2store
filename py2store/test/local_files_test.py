5b9c17037eb42c540d19cec197104d615c230279
Thor Whalen <thorwhalen1@gmail.com>
2020-10-30 14:30:02 -0700

    make a test for FileReader

---
+++ py2store/test/local_files_test.py
@@ -0,0 +1,21 @@
+from py2store import FileReader
+from py2store.test import minifs_dirpath, minifs_join
+
+
+def test_file_reader():
+    # Test it "raw"
+    s = FileReader(minifs_dirpath)
+    assert list(s) == [minifs_join(f) for f in ['x.bin', 'A/', 'C/', 'B/']]
+
+    # Now, we'll use mk_relative_path_store to make the tests more "natural"
+    # (and test it's interaction with mk_relative_path_store)
+    from py2store import mk_relative_path_store
+    s = mk_relative_path_store(FileReader(minifs_dirpath), prefix_attr='rootdir')
+    assert list(s) == ['x.bin', 'A/', 'C/', 'B/']  # that works!
+    assert s['x.bin'] == b'contents of x'
+    ss = s['A/']
+    assert isinstance(ss, FileReader)
+
+    # FIXME: The mk_relative_path_store wrapper doesn't play well with the recursion.
+    #   Here, we're back to absolute paths. Make it work!
+    list(ss) == [minifs_join('A/a')]  # we would like it to be 'A/a' simply
