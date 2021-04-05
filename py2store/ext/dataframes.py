585c3b62f21914adbf09ab2a9f0dbf56dd4eba0b
Thor Whalen <thorwhalen1@gmail.com>
2021-04-05 11:51:48 -0700

    made some forward-looking changes to dataframes.py

--- py2store/ext/dataframes.py
+++ py2store/ext/dataframes.py
@@ -8,8 +8,11 @@ import pickle
 with ModuleNotFoundErrorNiceMessage():
     import pandas as pd
 
+DFLT_EXT_SPECS = {}
 
-def df_from_data_given_ext(data, ext, **kwargs):
+
+def df_from_data_given_ext(data, ext, ext_specs=None, **kwargs):
+    ext_specs = ext_specs or DFLT_EXT_SPECS  # NOTE: Note used yet
     if ext.startswith("."):
         ext = ext[1:]
     if ext in {"xls", "xlsx"}:
@@ -36,21 +39,29 @@ def df_from_data_given_ext(data, ext, **kwargs):
 # TODO: Make the logic independent from local files assumption.
 # TODO: Better separate Reader, and add DfStore to make a writer.
 
+from functools import partial
 
-class DfReader(LocalBinaryStore):
+
+class DfLocalFileReader(LocalBinaryStore):
     def __init__(self, path_format, ext_specs=None):
         super().__init__(path_format)
         if ext_specs is None:
-            ext_specs = {}
-        self.ext_specs = ext_specs
+            ext_specs = DFLT_EXT_SPECS
+        self._ext_specs = ext_specs
+        self.data_and_ext_to_df = partial(df_from_data_given_ext, ext_specs=ext_specs)
+        self.data_and_ext_to_df = df_from_data_given_ext  # TODO: Hard coded for now, to keep functioning
 
     def __getitem__(self, k):
-        _, ext = os.path.splitext(k)
-        if ext.startswith("."):
-            ext = ext[1:]
+        ext = self.key_to_ext(k)
         kwargs = self.ext_specs.get(ext, {})
         data = BytesIO(super().__getitem__(k))
         return df_from_data_given_ext(data, ext, **kwargs)
+
+    def key_to_ext(self, k):
+        _, ext = os.path.splitext(k)
+        if ext.startswith("."):
+            ext = ext[1:]
+        return ext
 
     def __setitem__(self, k, v):
         raise NotImplementedError(
@@ -61,3 +72,6 @@ class DfReader(LocalBinaryStore):
         raise NotImplementedError(
             "This is a reader: No delete operation allowed"
         )
+
+
+DfReader = DfLocalFileReader  # alias for back-compatibility: TODO: Issue warning on use
